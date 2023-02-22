64f00f65921cb61620685395a9008aff05bb22e4
Edouard de Montchenu edouarddemontchenu@gmail.com <edouarddemontchenu@gmail.com>
2023-02-22 16:41:48 +0100

    resto review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Coffee.blend_name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::Number.leading_zero_number,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+  )
+  restaurant.save!
+  puts 'It is all good'
+end
